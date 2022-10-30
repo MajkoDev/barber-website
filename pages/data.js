13957be0b4d235ea6c3122f60fc1d8b07989f7eb
Marian <hellomariandev@gmail.com>
2022-10-30 22:41:10 +0100

    data file

---
+++ pages/data.js
@@ -0,0 +1,96 @@
+// import images
+// import icons
+
+export const navData = {
+  items: [
+    { href: "/", name: "Domov" },
+    { href: "/about", name: "O nás" },
+    { href: "/services", name: "Ponuka" },
+    { href: "/barbers", name: "Barberi" },
+  ],
+};
+
+export const navButton = {
+  items: [
+    { href: "/booking", name: "Rezervácia" },
+    { href: "/contact", name: "Kontakt" },
+  ],
+};
+
+export const heroData = {
+  title: "A small business is only was good as its tools.",
+  subtitle:
+    "We’re different. Flex is the only saas business platform that lets you run your business on one platform, seamlessly across all digital channels.",
+  btnPrimaryText: "zisti viac o nás",
+  btnSecondaryText: "cenník",
+};
+
+export const aboutData = {
+  title: "Gain more insight into how people use your",
+  subtitle: " o nás",
+  text:
+    "With our integrated CRM, project management, collaboration and invoicing capabilities, you can manage every aspect of your business in one secure platform.",
+  btnText: "Get Started",
+};
+
+export const valueData = {
+  subtitle: "values",
+  title: "Gain more insight into how people use your",
+  text:
+    "With our integrated CRM, project management, collaboration and invoicing capabilities, you can manage every aspect of your business in one secure platform.",
+  values: [{ icon, title, subtitle }],
+};
+
+export const serviceData = {
+  subtitle: "services",
+  title: "Join our team",
+  text:
+    "Our philosophy is simple — hire a team of diverse, passionate people and foster a culture that empowers you to do you best work.",
+  items: [
+    {
+      title: "Pánsky strih",
+      price: "15.00 €",
+      time: "30-60 min.",
+      description: "klasický pánsky strih, strihanie nožnicami, strojčekom",
+    },
+  ],
+};
+
+export const barberData = {
+  subtitle: "barberi",
+  title: "Meet our team",
+  text:
+    "Highly professional and capable of running your business across all digital channels.",
+  persons: [
+    {
+      name: "Macauley Herring",
+      subtitle: "Externý barber",
+      instagram: "https://www.instagram.com/",
+    },
+  ],
+};
+
+export const contactData = {
+  subtitle: "contact",
+  title: "Let’s stay connected",
+  text:
+    "It's never been easier to get in touch with Flex. Call us, use our live chat widget or email and we'll get back to you as soon as possible!",
+  contacts: [
+    { title: "email", subtitle: "contact@now.com" },
+    { title: "phone", subtitle: "0900 112 221" },
+    { title: "socials", instagram: "https://www.instagram.com/" },
+  ],
+};
+
+export const footerData = {
+  items: [
+    { href: "/", name: "Domov" },
+    { href: "/about", name: "O nás" },
+    { href: "/services", name: "Ponuka" },
+    { href: "/barbers", name: "Barberi" },
+  ],
+  buttons: [
+    { href: "/booking", name: "Rezervácia" },
+    { href: "/contact", name: "Kontakt" },
+  ],
+};
