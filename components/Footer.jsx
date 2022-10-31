a25495566756584b141e622c53bdb3ac38f21484
Marian <hellomariandev@gmail.com>
2022-10-31 18:04:24 +0100

    footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,25 @@
+import { footerData } from "../pages/data";
+
 const Footer = () => {
+  const { title, items } = footerData;
+
   return (
-    <div>
-      <h2 className="bg-gray-400 text-white text-xl font-thin">Footer</h2>
-    </div>
+    <footer className="bg-gray-800 text-amber-50 ">
+      <h2 className="text-2xl font-bold">{title}</h2>
+      <ul className="flex">
+        {items.map((item, index) => {
+          return (
+            <li key={index}>
+              <a href={item.href} className='m-2 font-light'>{item.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="">
+
+      </div>
+      <p className="text-medium font-thin mt-4">© 2021 Flex. All rights reserved.</p>
+    </footer>
   );
 };
 
