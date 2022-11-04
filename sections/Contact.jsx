5ee20f9e53de2abbafee1b6d01dedd9f03e19d62
Marian <hellomariandev@gmail.com>
2022-11-04 23:01:04 +0100

    contact

--- sections/Contact.jsx
+++ sections/Contact.jsx
@@ -1,13 +1,25 @@
-import Map from '../components/Map'
+import Map from "../components/Map";
 
 const Contact = () => {
-    return (
-      <div>
-        <h1 className="text-5xl font-extralight mb-6">Contact Section</h1>
-        <Map />
+  return (
+    <div id='contact' className="section">
+      <h1 className="text-2xl font-extralight mb-6">Contact</h1>
+
+      <div className="intro">
+        <h2 className="subtitle"></h2>
+        <h1 className="title"></h1>
+        <p className="description"></p>
       </div>
-    );
-  };
-  
-  export default Contact;
-  
+
+      <div className="options">
+        <div className="phone">Phone</div>
+        <div className="mail">Email</div>
+        <div className="socials">Socials</div>
+      </div>
+
+      <Map />
+    </div>
+  );
+};
+
+export default Contact;
