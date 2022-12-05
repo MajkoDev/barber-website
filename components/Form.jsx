c50711178bcd26f87f55774e1fb9878759231e30
Marian <hellomariandev@gmail.com>
2022-12-05 20:07:06 +0100

    form component

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,36 +1,110 @@
 const Form = () => {
   return (
-    <div className="max-w-xl w-full">
-      <form className="bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4">
-        <div className="flex flex-col">
-          <div className="gap-2 flex flex-1">
-            <input
-              className=" shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="name"
-              type="text"
-              placeholder="Name"
-            ></input>
-            <input
-              className=" shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              id="surname"
-              type="text"
-              placeholder="Surname"
-            ></input>
-          </div>
+    <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md">
+      <form>
+        <div class="form-group mb-6">
           <input
-            className="my-2 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email"
             type="text"
-            placeholder="Email"
-          ></input>
-          <div class="flex justify-center">
-            
-
-
-
-   
-</div>
+            class="form-control block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            id="exampleInput7"
+            placeholder="Name"
+          />
         </div>
+        <div class="form-group mb-6">
+          <input
+            type="email"
+            class="form-control block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+            id="exampleInput8"
+            placeholder="Email address"
+          />
+        </div>
+        <div class="form-group mb-6">
+          <textarea
+            class="
+          form-control
+          block
+          w-full
+          px-3
+          py-1.5
+          text-base
+          font-normal
+          text-gray-700
+          bg-white bg-clip-padding
+          border border-solid border-gray-300
+          rounded
+          transition
+          ease-in-out
+          m-0
+          focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+        "
+            id="exampleFormControlTextarea13"
+            rows="3"
+            placeholder="Message"
+          ></textarea>
+        </div>
+        <div class="form-group form-check text-center mb-6">
+          <input
+            type="checkbox"
+            class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+            id="exampleCheck87"
+            checked
+          />
+          <label
+            class="form-check-label inline-block text-gray-800"
+            for="exampleCheck87"
+          >
+            Send me a copy of this message
+          </label>
+        </div>
+        <button
+          type="submit"
+          class="
+        w-full
+        px-6
+        py-2.5
+        bg-blue-600
+        text-white
+        font-medium
+        text-xs
+        leading-tight
+        uppercase
+        rounded
+        shadow-md
+        hover:bg-blue-700 hover:shadow-lg
+        focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+        active:bg-blue-800 active:shadow-lg
+        transition
+        duration-150
+        ease-in-out"
+        >
+          Send
+        </button>
       </form>
     </div>
   );
