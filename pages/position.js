90af4a70da6a3a94500f3ae0c0244a49b3ac7b66
Marian <hellomariandev@gmail.com>
2022-12-13 21:35:57 +0100

    update

---
+++ pages/position.js
@@ -0,0 +1,22 @@
+export default function Position() {
+  return (
+    <div className="bg-gray-50">
+      <div className="container h-[60vh]">
+        <h1>Hello World</h1>
+      </div>
+
+      <div className="h-[100vh]"></div>
+      <div className="h-[100vh]"></div>
+      <div className="h-[100vh]"></div>
+
+
+
+      <div className="h-[100vh] flex justify-center items-start pt-12 bg-slate-100">
+        <h2 className="text-2xl font-semibold bg-slate-500 px-3">Section</h2>
+      </div>
+
+
+
+    </div>
+  );
+}
