2dc595a856945258c526758d4c1aa585710bbd2d
Marian <hellomariandev@gmail.com>
2022-12-30 17:29:15 +0100

    card of barber

--- components/BarberCard.jsx
+++ components/BarberCard.jsx
@@ -1,10 +1,28 @@
+import { barberData } from "../pages/data";
+
 const BarberCard = () => {
+  const { persons } = barberData;
+
+  const list = persons.map((person, index) => {
+    <p>{person.subtitle}</p>
+
+  })
+
   return (
-    <div className="m-2">
-      
-      <h3 className="font-semibold text-xl">BarberCard</h3>
+    <div className="m-2 border w-64 p-4 text-center">
+      <div class="w-full lg:px-8 p-4 md:p-0 bg-gray-600 h-[22rem]">
+      </div>
+
+      <h3 className="font-semibold text-xl mt-2">Barber Card {list}</h3>
+      {list}
     </div>
   );
 };
 
 export default BarberCard;
+
+{
+  /*
+        className="section h-[100vh] flex items-center justify-center bg-fixed bg-center bg-cover custom-img"
+*/
+}
