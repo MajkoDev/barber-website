88d03261b4f4f846c48419c32ec935d2d4225b72
Marian <hellomariandev@gmail.com>
2023-01-04 18:28:31 +0100

    update

--- sections/contact/Data.js
+++ sections/contact/Data.js
@@ -2,21 +2,26 @@ const Data = () => {
   return (
     <section class="text-gray-600 body-font">
       <div class="container px-5 py-24 mx-auto">
-        <div class="flex flex-wrap -m-4">
-          <div class="p-4 lg:w-1/3">
-            <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                KATEGORIA
+
+        <div class="flex flex-wrap -m-4 justify-center">
+
+          <div class="p-4 lg:w-1/3 md:w-1/2">
+          <div class="h-full bg-gray-100 bg-opacity-75 px-8 pb-24 rounded-lg overflow-hidden text-center relative pt-4">
+
+              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-2">
+                ZAVOLAJTE NÁM
               </h2>
-              <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                Zavolajte nám ihneď zo svojho mobilu.
+              <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-2">
+                Telefónne číslo{" "}
               </h1>
-              <p class="leading-relaxed mb-3">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
+              <p class="leading-relaxed text-md mb-4 ">
+                Môžete nám kedykoľvek zavolať a <br /> dohonúť si termín priamo.
               </p>
+
+              <a href="text-green-500 form-semibold text-3xl m-2">0000 333 222</a>
+
+              <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
               <a class="text-green-500 inline-flex items-center">
-                Learn More
                 <svg
                   class="w-4 h-4 ml-2"
                   viewBox="0 0 24 24"
@@ -30,7 +35,6 @@ const Data = () => {
                   <path d="M12 5l7 7-7 7"></path>
                 </svg>
               </a>
-              <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
                 <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
                   <svg
                     class="w-4 h-4 mr-1"
@@ -64,18 +68,82 @@ const Data = () => {
             </div>
           </div>
 
-          <div class="p-4 lg:w-1/3">
-            <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+          <div class="p-4 lg:w-1/3 md:w-1/2">
+          <div class="h-full bg-gray-100 bg-opacity-75 px-8 pb-24 rounded-lg overflow-hidden text-center relative pt-4">
+
+              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-2">
+                ZAVOLAJTE NÁM
+              </h2>
+              <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-2">
+                Telefónne číslo{" "}
+              </h1>
+              <p class="leading-relaxed text-md mb-4 ">
+                Môžete nám kedykoľvek zavolať a <br /> dohonúť si termín priamo.
+              </p>
+
+              <a href="text-green-500 form-semibold text-3xl m-2">0000 333 222</a>
+
+              <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+              <a class="text-green-500 inline-flex items-center">
+                <svg
+                  class="w-4 h-4 ml-2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <path d="M5 12h14"></path>
+                  <path d="M12 5l7 7-7 7"></path>
+                </svg>
+              </a>
+                <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                  <svg
+                    class="w-4 h-4 mr-1"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                    <circle cx="12" cy="12" r="3"></circle>
+                  </svg>
+                  1.2K
+                </span>
+                <span class="text-gray-400 inline-flex items-center leading-none text-sm">
+                  <svg
+                    class="w-4 h-4 mr-1"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                  </svg>
+                  6
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-4 lg:w-1/3 md:w-1/2">
+          <div class="h-full bg-gray-100 bg-opacity-75 px-8 pb-24 rounded-lg overflow-hidden text-center relative pt-4">
+          <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-2">
                 KATEGORIA
               </h2>
-              <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+              <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-2">
                 Napíšte nám mail so svojimi otázkami.
               </h1>
               <p class="leading-relaxed mb-3">
                 Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
                 microdosing tousled waistcoat.
               </p>
+              
               <a class="text-green-500 inline-flex items-center">
                 Learn More
                 <svg
@@ -125,66 +193,7 @@ const Data = () => {
             </div>
           </div>
 
-          <div class="p-4 lg:w-1/3">
-            <div class="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-              <h2 class="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                KATEGORIA
-              </h2>
-              <h1 class="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                Sledujte nás na sociálnych sieťach
-              </h1>
-              <p class="leading-relaxed mb-3">
-                Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
-                microdosing tousled waistcoat.
-              </p>
-              <a class="text-green-500 inline-flex items-center">
-                Learn More
-                <svg
-                  class="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M5 12h14"></path>
-                  <path d="M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-              <div class="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-                <span class="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                  <svg
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-                    <circle cx="12" cy="12" r="3"></circle>
-                  </svg>
-                  1.2K
-                </span>
-                <span class="text-gray-400 inline-flex items-center leading-none text-sm">
-                  <svg
-                    class="w-4 h-4 mr-1"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-                  </svg>
-                  6
-                </span>
-              </div>
-            </div>
-          </div>
+        
         </div>
       </div>
     </section>
