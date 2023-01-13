28ed395804e54df330881567222e01876d6ae8ea
Marian <hellomariandev@gmail.com>
2023-01-13 23:14:45 +0100

    Update Map.js

--- sections/contact/Map.js
+++ sections/contact/Map.js
@@ -16,12 +16,10 @@ const Map = () => {
 
       <div class="container px-5 py-24 mx-auto flex ">
       <div class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
-          <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">
-            Feedback
-          </h2>
-          <p class="leading-relaxed mb-5 text-gray-600">
-            Post-ironic portland shabby chic echo park, banjo fashion axe
-          </p>
+          
+          <h2 class="text-gray-900 text-xl mb-1 font-medium title-font text-center">Adresa</h2>
+          <p class="leading-relaxed mb-5 text-gray-600 text-center">Kde nás najdete?</p>
+
           <div class="relative mb-4">
             <label for="email" class="leading-7 text-sm text-gray-600">
               Email
